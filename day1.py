b37b59329c99a7e8963af5da48095c4e89cfe0d0
Sasankalluru <sasankvarma5050@gmail.com>
2026-07-28 14:59:44 +0530

    day-1

---
+++ day1.py
@@ -0,0 +1,33 @@
+print('Hello codegnan')
+'''
+Tokens -->  A smallest unit in a program
+'''
+name = 'codegnan'
+print(name)
+
+emailid = 'sasankvarma5050@gmail.com'
+print(emailid)
+
+email_id = 'sasankvarma5050@gmail.com'
+print(email_id)
+
+hunter='350'
+print(hunter)
+
+batch = 'DA23'
+print(batch)
+
+batch = 'cse'
+print(batch)
+print(batch)
+
+length = 5
+breadth =10
+area = length * breadth
+print(area)
+
+name,location,age= 'sasank','hyd','21'
+print(name)
+print(name)
+print(location)
+print(age)
